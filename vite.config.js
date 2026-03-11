0be9eb18d53048294c1bc5c25eda429da8b71f75
Hari <hariharakrishnan.official@gmail.com>
2026-03-11 16:28:06 +0530

    Auth updated

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:9000',
+        target: 'https://railway-ticketing-system-50039510865.development.catalystappsail.in',
         changeOrigin: true,
       }
     }
